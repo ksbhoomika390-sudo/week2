adc44d94d641d8b24a708297a80f7bd6e08e8598
ksbhoomika390-sudo <ksbhoomika390@gmail.com>
2026-02-11 22:03:52 +0530

    Add files via upload

---
+++ week2/Day2/set_operation.py
@@ -0,0 +1,7 @@
+set1 = {10, 20, 30, 40}
+set2 = {30, 40, 50, 60}
+print("Union:", set1 | set2)
+print("Intersection:", set1 & set2)
+print("Difference (set1 - set2):", set1 - set2)
+print("Difference (set2 - set1):", set2 - set1)
+print("Symmetric Difference:", set1 ^ set2)
